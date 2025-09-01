69a34b41d94ad6c141ae3392a78bd2608ac86457
digiloudis <christodigiloudis@gmail.com>
2025-09-01 20:15:21 +0300

    Remove dist/ from repository

--- dist/types/icons/ArrowLeftIcon.d.ts
+++
@@ -1,3 +0,0 @@
-import * as types from '@/types';
-declare function ArrowLeftIcon({ size, color, onPress }: types.Parameters): types.Component;
-export default ArrowLeftIcon;
--- dist/types/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './Parameters';
-export * from './Component';
